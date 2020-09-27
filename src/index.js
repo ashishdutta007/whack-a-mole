9a88a9d723c0ea4eb59b28bfce474d4c3003e312
Ashish Dutta <ashish.dutta050@gmail.com>
2020-09-28 01:55:46 +0530

    game init

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import "./styles.css";
 
 // document.addEventListener("DOMContentLoaded", () => {
 console.log("hi");
-alert("hey");
+// alert("hey");
 (function createGameGrid() {
-  let grid;
+  let grid = [];
   for (let i = 0; i < 9; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     grid.push(square);
   }
-  const container = document.getElementById("gameGrid");
-  debugger;
+  const container = document.querySelector(".gameGrid");
   console.log(grid);
   container.append(...grid);
 })();
 // });
+//
